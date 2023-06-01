aa83f6af460889d072e522103288374ce7c747f8
lukaszdyszy <lukaszdyszy0192@gmail.com>
2023-06-01 08:53:17 +0200

    more types

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,9 @@ export const BLACK_BISHOP = 'b';
 export const BLACK_QUEEN = 'q';
 export const BLACK_KING = 'k';
 
-export enum WhatePiece {
+export const EMPTY = '';
+
+export enum WhitePiece {
 	PAWN = WHITE_PAWN,
 	ROOK = WHITE_ROOK,
 	KNIGHT = WHITE_KNIGHT,
@@ -30,11 +32,39 @@ export enum BlackPiece {
 	KING = BLACK_KING,
 }
 
-export type Piece = `${WhatePiece | BlackPiece}`;
+export type Piece = `${WhitePiece | BlackPiece}`;
 
 export type RowName = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8';
 export type ColName = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h';
 export type FieldName = `${ColName}${RowName}`;
+export type FieldCoords = [number, number];
+export type FieldValue = Piece | '';
 
+export type MoveString = `${FieldName}-${FieldName}`;
+export type MoveName = `${WhitePiece}${FieldName}-${FieldName}`;
+export type Move = [FieldCoords, FieldCoords];
 
-export class Chesslogic { }
+export type Board = [
+	[FieldValue, FieldValue, FieldValue, FieldValue, FieldValue, FieldValue, FieldValue, FieldValue],
+	[FieldValue, FieldValue, FieldValue, FieldValue, FieldValue, FieldValue, FieldValue, FieldValue],
+	[FieldValue, FieldValue, FieldValue, FieldValue, FieldValue, FieldValue, FieldValue, FieldValue],
+	[FieldValue, FieldValue, FieldValue, FieldValue, FieldValue, FieldValue, FieldValue, FieldValue],
+	[FieldValue, FieldValue, FieldValue, FieldValue, FieldValue, FieldValue, FieldValue, FieldValue],
+	[FieldValue, FieldValue, FieldValue, FieldValue, FieldValue, FieldValue, FieldValue, FieldValue],
+	[FieldValue, FieldValue, FieldValue, FieldValue, FieldValue, FieldValue, FieldValue, FieldValue],
+	[FieldValue, FieldValue, FieldValue, FieldValue, FieldValue, FieldValue, FieldValue, FieldValue]
+];
+
+export const STARING_POSITION = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+
+export const isValidFen = (fen: string): boolean => {
+	// TODO
+
+	return true;
+}
+
+export class Chesslogic {
+	constructor(FEN: string | null = null) {
+
+	}
+}
